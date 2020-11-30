1dc00e7c2ccb4353d6efb2ffbe66cb675bf482da
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-11-30 00:19:02 -0500

    Split startup extension in two

--- src/ForEvolve.OperationResults.AspNetCore/OperationResultStartupExtensions.cs
+++ src/ForEvolve.OperationResults.AspNetCore/OperationResultStartupExtensions.cs
@@ -1,6 +1,4 @@
-﻿using ForEvolve.OperationResults;
-using ForEvolve.OperationResults.AspNetCore;
-using ForEvolve.OperationResults.Standardizer;
+﻿using ForEvolve.OperationResults.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class OperationResultStartupExtensions
+    public static class OperationResultAspNetCoreStartupExtensions
     {
         /// <summary>
         /// Adds operation results Asp.Net Core filters.
@@ -19,44 +17,13 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </summary>
         /// <param name="services">The services.</param>
         /// <returns>IServiceCollection.</returns>
-        public static IServiceCollection AddForEvolveOperationResultFilters(this IServiceCollection services)
+        public static IServiceCollection AddForEvolveOperationResultModelBinderErrorActionFilter(this IServiceCollection services)
         {
             services.AddSingleton<ModelBinderErrorActionFilter>();
             services.Configure<MvcOptions>(options =>
             {
                 options.Filters.AddService<ModelBinderErrorActionFilter>();
             });
-            return services;
-        }
-
-        /// <summary>
-        /// Adds the default ForEvolve operation result standardizer filters.
-        /// </summary>
-        /// <param name="services">The services.</param>
-        /// <returns>IServiceCollection.</returns>
-        public static IServiceCollection AddForEvolveOperationResultStandardizer(this IServiceCollection services)
-        {
-            services
-                .AddLogging()
-                .AddSingleton<IPropertyNameFormatter, DefaultPropertyNameFormatter>()
-                .AddSingleton<IPropertyValueFormatter, DefaultPropertyValueFormatter>()
-                .AddSingleton<IOperationResultStandardizer, DefaultOperationResultStandardizer>()
-                .AddOptions<DefaultOperationResultStandardizerOptions>()
-            ;
-            services
-                .Configure<MvcOptions>(options =>
-                {
-                    options.Filters.Add<OperationResultStandardizerActionFilter<CreatedAtActionResult>>();
-                    options.Filters.Add<OperationResultStandardizerActionFilter<CreatedAtRouteResult>>();
-                    options.Filters.Add<OperationResultStandardizerActionFilter<CreatedResult>>();
-
-                    options.Filters.Add<OperationResultStandardizerActionFilter<OkObjectResult>>();
-
-                    options.Filters.Add<OperationResultStandardizerActionFilter<BadRequestObjectResult>>();
-                    options.Filters.Add<OperationResultStandardizerActionFilter<ConflictObjectResult>>();
-                    options.Filters.Add<OperationResultStandardizerActionFilter<NotFoundObjectResult>>();
-                    options.Filters.Add<OperationResultStandardizerActionFilter<UnprocessableEntityObjectResult>>();
-                });
             return services;
         }
     }
